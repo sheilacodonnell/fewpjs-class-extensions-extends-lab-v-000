ed747a783e46954c3c6c384e7f06dd49aed39b14
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-30 19:34:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Your code here
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+ 
+  get countSides() {
+    return this.sides.length;
+  }
+  
+  get perimeter() {
+    return this.sides.reduce((a,b) => a + b, 0)
+  }
+}
+
+class Triangle extends Polygon {
+
+  get isValid() {
+    let [a, b, c] = this.sides;
+    if ((a + b > c && a + c > b && b + c > a) ) {
+      return true
+    }
+    else
+      return false
+    }
+  }
+
+class Square extends Polygon {
+  
+  get area() {
+    return this.sides[0] * this.sides[0]
+  }
+  
+  get isValid() {
+    let squareSides = this.sides
+    let result = squareSides.every(function (e) {
+      return squareSides[0] === e;
+    }
+  )
+    return result
+  }
+}
+
